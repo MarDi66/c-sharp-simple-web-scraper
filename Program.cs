03af95cda1eec2e01b5e85ee1199fbbabcac5ab4
MarDi66 <yohann.mtfpro@gmail.com>
2020-01-02 10:25:25 +0100

    Add data folders and classes

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace SimpleWebScraper
     {
         static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            string google = client.DownloadString("http://google.com");
-            Console.WriteLine(google);
+            
         }
     }
 }
